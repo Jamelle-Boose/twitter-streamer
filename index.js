62e4e0aeb0dba9633071eb272f1b09f7c06c8cd8
Jamelle Boose <19831942+Jamelle-Boose@users.noreply.github.com>
2022-06-23 03:28:20 -0700

    add index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+// Setup access to Twitter and Google APIs using env variables
+
+// Setup a rule based filter on the search endpoint. It uses a company name and a hashtag from the environment and adds a rule to the search.
+
+// Only add this filter rule if it is not already defined.
+
+// Transform a tweet's data from the stream into a Google Spreadsheet row
+// Add the transformed data as a new row in the sheet
+
+// Listen to a stream of tweets from Twitter's API
+
+// Open a stream with the tweet data we are interested in
+
+// As tweets stream in - transform and store them
+
+// Helper function to print an error message and terminate
+
+// Helper function to add a rule to the stream
+
+// Start the program
