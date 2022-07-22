41f65986e1bbb3d11a3ebcd5eb914611eadead4d
Jamelle Boose <19831942+Jamelle-Boose@users.noreply.github.com>
2022-07-21 17:12:40 -0700

    Update blockedusers list

--- util/blocked_users.mjs
+++ util/blocked_users.mjs
@@ -1,3 +1,3 @@
-const blocked_users = ["yamanobudou8", "bluechipcard", "Interstocking", "PoundingDaTable", "intell_invest", "WifeyAlpha", "BaggerDon", "TWAOptions"]
+const blocked_users = ["yamanobudou8", "bluechipcard", "Interstocking", "PoundingDaTable", "intell_invest", "WifeyAlpha", "BaggerDon", "TWAOptions", "therichauntie", "TheFishPen"]
 
 export default blocked_users
